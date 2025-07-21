4436926e93940567a58c94537807e1f9c09e287e
kamal-bahaa <kamalbahaa53z@gmail.com>
2025-07-21 05:34:18 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { ProductComponent } from "./components/product/product.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavBarComponent, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
